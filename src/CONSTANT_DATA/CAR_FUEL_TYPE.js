1e3615ad78ea7ae09e860d851dd7d45e959ef642
kimjuno97 <jh241093@gmail.com>
2022-11-03 02:11:04 +0900

    fix: eslint 에러 수정

--- src/CONSTANT_DATA/CAR_FUEL_TYPE.js
+++ src/CONSTANT_DATA/CAR_FUEL_TYPE.js
@@ -1,7 +1,7 @@
 const CAR_FUEL_TYPE = {
   gasoline: '가솔린',
   ev: '전기',
-  hybrid: '하이브리드'
+  hybrid: '하이브리드',
 };
 
 export default CAR_FUEL_TYPE;
--- src/CONSTANT_DATA/COMMON_COLOR.js
+++ src/CONSTANT_DATA/COMMON_COLOR.js
@@ -2,7 +2,7 @@ const COMMON_COLOR = {
   BLACK: '#000000',
   GRAY: '#D9D9D9',
   BLUE: '#0094FF',
-  WHITE: '#FFFFFF'
+  WHITE: '#FFFFFF',
 };
 
 export default COMMON_COLOR;
